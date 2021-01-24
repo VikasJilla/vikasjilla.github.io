88dcb98e33621a6c10498c3f8cd62c955671007a
vikas jilla <jillavikas@gmail.com>
2021-01-24 18:57:30 +0530

    revamped UI

---
+++ js/index.js
@@ -0,0 +1,30 @@
+function onOptionSelected(){
+    var x = document.getElementsByClassName("selected");
+    for(var i =0; i < x.length;i++){
+        x[i].classList.remove("selected");
+    }
+}
+
+$(document).click(function(event) {
+    var element = event.target;
+    console.log(element.classList);
+    if(element.classList.contains("option")){
+        if(element.classList.contains("selected"))return;
+        element.classList.add("selected");
+    }
+    var eid = element.id;
+    console.log("element id "+eid);
+    if(eid == "linkedin") window.open("https://www.linkedin.com/in/vikas-jilla-95356657");
+    else if(eid == "github") window.open("https://github.com/VikasJilla");
+    else if(eid == "wordpress") window.open("https://vikasjilla.wordpress.com");
+});
+
+window.addEventListener("resize", onWindowResize);
+function onWindowResize(){
+    var w = $(document).width()*0.2;
+    if(w<200){
+        document.getElementById("profilePhotoContainer").style.display = "contents";
+    }else{
+        document.getElementById("profilePhotoContainer").style.display = "flex";
+    }
+}
